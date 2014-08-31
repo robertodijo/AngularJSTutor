5abf5a57082b9a48b29a98adc945a682234b022e
Roberto Dijo <robertodijo@gmail.com>
2014-09-01 01:25:29 +0200

    commit, before moving to github

--- AngularJsTutor/apps/AngularJsTutor/common/js/controllers.js
+++ AngularJsTutor/apps/AngularJsTutor/common/js/controllers.js
@@ -1,6 +1,14 @@
 /**
  * 
  */
-function HelloController($scope){
-	$scope.greeting={text:'Roberto'};
-}
+var myAppModule=angular.module('myApp', []);
+
+var messages = {};
+messages.greetingText="Roberto";
+messages.long="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac pellentesque lacus, nec molestie enim. Duis vitae molestie augue.  Nunc vestibulum pharetra vehicula. Aenean non quam volutpat, placerat metus ac,  consectetur libero.";
+messages.short="Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+messages.introduction = messages.long;
+myAppModule.controller('TextController',
+function($scope){
+	$scope.messages=messages;
+});
